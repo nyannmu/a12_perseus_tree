5d02d0a49aa3c92e96720be8cb34cb54e9333330
lolipuru <banstellazz@gmail.com>
2021-10-18 01:49:02 +0700

    Revert "perseus: redfin oct. fingerprint"
    This reverts commit 22139f2b3e66bfd41645851032d7994b5e20970d.

--- xdroid_perseus.mk
+++ xdroid_perseus.mk
@@ -20,10 +20,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/perseus/perseus:10/QKQ1.190828.002/V11.0.3.0.QEEMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001.A1 7641976 release-keys" \
+    PRIVATE_BUILD_DESC="perseus-user 10 QKQ1.190828.002 V11.0.3.0.QEEMIXM release-keys" \
     PRODUCT_NAME="perseus"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
