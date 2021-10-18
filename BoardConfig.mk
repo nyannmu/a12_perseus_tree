a186d7db305876ad2e26b7e6a7eea6598ad06a3d
lolipuru <banstellazz@gmail.com>
2021-10-19 03:26:30 +0700

    perseus: add init script
    for safetynet sake
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,10 @@ TARGET_USES_NQ_NFC := true
 # Inherit from the proprietary version
 -include vendor/xiaomi/perseus/BoardConfigVendor.mk
 
+# Vendor init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_perseus
+TARGET_RECOVERY_DEVICE_MODULES := libinit_perseus
+
 SELINUX_IGNORE_NEVERALLOWS := true
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
